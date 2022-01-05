1f276ed0e23bc73b2da2d9494bce086bb6af4d80
egarcia75 <eginfobyte@gmail.com>
2022-01-05 16:12:58 -0300

    comienzo del proyecto

---
+++ js/script.js
@@ -0,0 +1,18 @@
+class Persona {
+
+    constructor (nombre, apellido, edad) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+    }
+
+    saludar (){
+        alert(`Hola ${this.nombre} ${this.apellido}, tienes ${this.edad} años`);
+    }
+
+}
+
+/* eduardo = new Persona('Eduardo', 'Garcia', 46);
+eduardo.saludar();
+carlos = new Persona('Carlos', 'Pedraza', 52);
+carlos.saludar(); */
